a63470353176f702a5a13faba280d30d4f004571
Arpit Jain <arpit.jain16@gmail.com>
2013-07-30 03:18:49 +0000

    input from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var buffer = new Buffer();
+  	buffer = fs.readFileSync("index.html");
+	response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
