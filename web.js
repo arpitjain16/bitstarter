6b10fe27edde0a1e55b1b0739562ba7caacd8915
Arpit Jain <arpit.jain16@gmail.com>
2013-08-09 17:06:07 +0000

    typo removed

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
 	var buffer = new Buffer(8);
   	buffer = fs.readFileSync("index.html");
-	response.send(buffer.toString);
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
